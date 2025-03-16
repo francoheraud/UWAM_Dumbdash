3c0a6369f0f853c24fb8b7cda08c4b1ad26fcbfe
Franco Heraud <francoheraud@Outlook.com>
2025-03-16 15:39:13 +0800

    Update can.h

--- can.h
+++ can.h
@@ -43,43 +43,32 @@ typedef struct {
 } TickManager_TypeDef;
 
 
-typedef struct {
 
-	//CAN_InitTypeDef *can_init;
-	//UART_HandleTypeDef *uart_handle;
-	CAN_HandleTypeDef *can_handle;
-	CAN_TxHeaderTypeDef *tx_header;
-	CAN_RxHeaderTypeDef *rx_header;
-	CAN_FilterTypeDef *filter_header;
+typedef struct {
 
 	TickManager_TypeDef tick_t;
 	FSM_DebounceStates current_state;
 
 	uint8_t tx_data[8];
 	uint8_t rx_data[8];
-	uint32_t tx_mailbox;
-	uint32_t rx_mailbox;
+	uint32_t id;
+	uint8_t len;
 
 	uint8_t prev_enc_pos_1;
 	uint8_t prev_enc_pos_2;
 
-
 } Dumbdash_TypeDef;
 
 
-
-HAL_StatusTypeDef Dumbdash_Transmit_CAN_Msg(Dumbdash_TypeDef *dev, uint32_t id, uint32_t length);
+HAL_StatusTypeDef Dumbdash_CAN_HAL_Init(CAN_HandleTypeDef *hcan, CAN_FilterTypeDef *filter_header);
+HAL_StatusTypeDef Dumbdash_Init(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_FilterTypeDef *filter_header);
+HAL_StatusTypeDef Dumbdash_Transmit_CAN_Msg(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_TxHeaderTypeDef *tx);
+HAL_StatusTypeDef Dumbdash_Receive_CAN_Msg(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_RxHeaderTypeDef *rx);
 uint8_t Dumbdash_Read_Rotary_Encoder_Outputs(const uint16_t *output_arr);
-void Dumbdash_CAN_HAL_Init(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *can_handle, CAN_TxHeaderTypeDef *tx_header, CAN_RxHeaderTypeDef *rx_header, CAN_FilterTypeDef *filter_header);
-void Dumbdash_Init(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *can_handle, CAN_TxHeaderTypeDef *tx_header, CAN_RxHeaderTypeDef *rx_header, CAN_FilterTypeDef *filter_header);
-HAL_StatusTypeDef Dumbdash_Launch_Control_Interrupt_FSM(Dumbdash_TypeDef *dev, volatile uint8_t data_ready_flag);
-//void Dumbdash_UART_TestFunc_Init(Dumbdash_TypeDef *dev, UART_HandleTypeDef *uart_handle);
-//void Dumbdash_Encoder_UART_TestFunc(Dumbdash_TypeDef *dev, HAL_StatusTypeDef status, UART_HandleTypeDef *uart_handle, uint8_t encoder_pos);
-HAL_StatusTypeDef Dumbdash_Transmit_Encoder_Data_v2(Dumbdash_TypeDef *dev);
-HAL_StatusTypeDef Dumbdash_Receive_CAN_Msg(Dumbdash_TypeDef *dev, uint32_t id, uint32_t length);
+HAL_StatusTypeDef Dumbdash_Transmit_Encoder_Data(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_TxHeaderTypeDef *tx);
+HAL_StatusTypeDef Test_Loopback(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_TxHeaderTypeDef *tx, CAN_RxHeaderTypeDef *rx);
+HAL_StatusTypeDef Dumbdash_Launch_Control_Interrupt_FSM(Dumbdash_TypeDef *dev, CAN_HandleTypeDef *hcan, CAN_TxHeaderTypeDef *tx, volatile uint8_t data_ready_flag);
+
 
 
 #endif /* INC_CAN_H_ */
-
-
-
